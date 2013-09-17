5665053941757d32885937d9e0abc935e155f534
Trevor <landautrevor@gmail.com>
2013-09-17 09:26:26 -0400

    es5ified

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+var window = {};
 (function (root) {
     'use strict';
 
@@ -11,41 +12,57 @@
     }
 
     Collection.prototype = {
-        // Track length of collections
-        length: 0,
-
-        _convertToArray: function (args) {
-            if (!Array.isArray(args) && arguments.length > 0) {
-                // Convert to array
-                if (typeof args.toArray === 'function') {
-                    args = args.toArray();
-                } else {
-                    args = __slice.apply(arguments); 
-                }
-            }
-            return args;
-        },
-
-        on: function (name, fn, ctx) {
-            // Event hash to holf events
-            this._events = this._events || {};
-            var events = this._events[name] || (this._events[name] = []);
-            events.push({ fn: fn, ctx: ctx || this });
-            return this;
-        },
-
-        trigger: function (name) {
-            if (!this._events) return this;
-            var args = __slice.call(arguments, 1);
-            var events = this._events[name];
-            if (events) {
-                events.forEach(function (ev) {
-                    ev.fn.apply(ev.ctx, args);
-                });
-            }
-            return this;
+        get size() {
+            return this.length
         }
     };
+
+    Object.defineProperties(Collection.prototype, {
+        // Track length of collections
+        length: {
+            value: 0,
+            enumerable: true,
+            configurable: true
+        },
+
+        _convertToArray: {
+            value: function (args) {
+                if (!Array.isArray(args) && arguments.length > 0) {
+                    // Convert to array
+                    if (typeof args.toArray === 'function') {
+                        args = args.toArray();
+                    } else {
+                        args = __slice.apply(arguments); 
+                    }
+                }
+                return args;
+            }
+        },
+
+        on: {
+            value: function (name, fn, ctx) {
+                // Event hash to holf events
+                this._events = this._events || {};
+                var events = this._events[name] || (this._events[name] = []);
+                events.push({ fn: fn, ctx: ctx || this });
+                return this;
+            }
+        },
+
+        trigger: {
+            value: function (name) {
+                if (!this._events) return this;
+                var args = __slice.call(arguments, 1);
+                var events = this._events[name];
+                if (events) {
+                    events.forEach(function (ev) {
+                        ev.fn.apply(ev.ctx, args);
+                    });
+                }
+                return this;
+            }
+        }
+    });
 
     var omit = ['length', 'constructor', 'toString', 'toLocalString'];
     var methods = Object.getOwnPropertyNames(arrProto)
@@ -54,46 +71,67 @@
         });
 
     methods.forEach(function (fn) {
-        Collection.prototype[fn] = function () {
-            var args = __slice.call(arguments);
-            var ret = arrProto[fn].apply(this, args);
-            args = [fn].concat(args);
-            this.trigger.apply(this, args);
-            return ret;
-        };
+        Object.defineProperty(Collection.prototype, fn, {
+            value: function () {
+                var args = __slice.call(arguments);
+                var ret = arrProto[fn].apply(this, args);
+                args = [fn].concat(args);
+                this.trigger.apply(this, args);
+                return ret;
+            }
+        });
     });
 
-
-    Collection.zip = function () {
-        var args = __slice.call(arguments);
-        var lengths = args.map(function (c) { return c.length; });
-        var length = Math.max(lengths.concat(0));
-        var results = new Array(length);
-        for (var i = 0; i < length; i += 1) {
-            results[i] = args.map(function (c) { return c[i]; });
-        }
-        return results;
-    };
 
     // Just incase another type of collection
     // exists on the window already
     var oldC = root.Collection;
     root.Collection = Collection;
-    Collection.noConflict = function () {
-        root.Collection = oldC;
-        return Collection;
-    };
+
+    Object.defineProperties(Collection, {
+        zip: {
+            value: function () {
+                var args = __slice.call(arguments);
+                var lengths = args.map(function (c) { return c.length; });
+                var length = Math.max(lengths.concat(0));
+                var results = new Array(length);
+                for (var i = 0; i < length; i += 1) {
+                    results[i] = args.map(function (c) { return c[i]; });
+                }
+                return results;
+            }
+        },
+        noConflict: {
+            value: function () {
+                root.Collection = oldC;
+                return Collection;
+            }
+        }
+    });
 })(window);
 
-var log = console.log;
-var Collection = Collection.noConflict();
+(function (root) {
+    'use strict';
+    var log = console.log;
+    var Collection = root.Collection.noConflict();
 
-var c = new Collection([1,2,3]);
-log(c, c.length === 3);
+    var c = new Collection([1,2,3]);
+    log(c, c.length === 3);
 
-c.on('push', function () {
-    log('push event', arguments);
-});
-c.push(4, 5, 6);
-log(c, c.length === 6);
+    c.on('push', function () {
+        log('push event', arguments);
+    });
+    c.push(4, 5, 6);
+    log(c, c.length === 6);
 
+    try {
+        Collection.zip = 'foo';
+    } catch (e) {
+        log(e.message);
+    }
+
+    console.log(c.size === c.length); // true
+    console.log(Collection.prototype); // { size: [Getter], length: 0 }
+    console.log(Collection.prototype.trigger); // [Function]
+
+})(window);
